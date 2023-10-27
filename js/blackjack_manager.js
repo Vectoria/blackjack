6163c57c97e4612875a4f2ea27e89bbe4c2d4335
deSoos <fsousa2004@gmail.com>
2023-10-27 18:24:53 +0100

    javascript update

--- js/blackjack_manager.js
+++ js/blackjack_manager.js
@@ -1,50 +1,81 @@
 //Blackjack oop
 
-let game = null;
+let game = null
 
 function debug(an_object) {
-    document.getElementById("debug").innerHTML = JSON.stringify(an_object);
+    document.getElementById("debug").innerHTML = JSON.stringify(an_object)
 }
 
 function buttons_initialization() {
-    document.getElementById("card").disabled = false;
-    document.getElementById("stand").disabled = false;
-    document.getElementById("new_game").disabled = true;
+    document.getElementById("card").disabled = false
+    document.getElementById("stand").disabled = false
+    document.getElementById("new_game").disabled = true
 }
 
 function finalize_buttons() {
-    document.getElementById("card").disabled = true;
-    document.getElementById("stand").disabled = true;
-    document.getElementById("new_game").disabled = false;
+    document.getElementById("card").disabled = true
+    document.getElementById("stand").disabled = true
+    document.getElementById("new_game").disabled = false
 }
 
 
 //FUNÇÕES QUE DEVEM SER IMPLEMENTADAS PELOS ALUNOS
 function new_game() {
-    game = new BlackJack();
-    debug(game);
-    player_new_card();
-    dealer_new_card();
-    dealer_new_card();
+    game = new BlackJack()
+    debug(game)
+    dealer_new_card()
+    dealer_new_card()
+    player_new_card()
 }
 
 function update_dealer(state) {
-    //player_move() ;
-    //if(state== )
-
+    let dealer_info = ""
+    if (state.gameEnded){
+        let dealer_cards = game.get_dealer_cards()
+        dealer_info += "Dealer Cards: "
+        for (let i = 0; i < dealer_cards.length; i++) {
+            dealer_info += dealer_cards[i].Value + " of " + dealer_cards[i].Suit
+            if (i < dealer_cards.length - 1) {
+                dealer_info += ", "
+            }
+        }
+        if (state.dealerWon){
+            dealer_info += "dealer wins"
+        } else {
+            dealer_info += "dealer loses"
+        }
+        document.getElementById("dealer_cards").innerHTML = dealer_info
+        finalize_buttons()
+    }
 }
 
 function update_player(state) {
-    
-    //dealer_move();
+    let player_info = ""
+        let player_cards = game.get_player_cards()
+        player_info += "Player Cards: "
+        if (state.gameEnded){
+            for (let i = 0; i < player_cards.length; i++) {
+            player_info += player_cards[i].Value + " of " + player_cards[i].Suit
+            if (i < player_cards.length - 1) {
+                player_info += ", "
+            }
+        }
+        if (state.dealerWon){
+            player_info += "player loses"
+        } else {
+            player_info += "player won"
+        }
+        document.getElementById("player_cards").innerHTML = player_info
+        finalize_buttons()
+    }
 }
 
 function dealer_new_card() {
-    update_dealer();
+    update_dealer()
 }
 
 function player_new_card() {
-    update_player();
+    update_player()
 }
 
 function dealer_finish() {
--- js/blackjack_object.js
+++ js/blackjack_object.js
@@ -1,18 +1,18 @@
 //Blackjack object
 
 //constante com o número máximo de pontos para blackJack
-const MAX_POINTS = 21;
-const MIN_POINTS_DEALER=17;
+const MAX_POINTS = 21
+const MIN_POINTS_DEALER=17
 
 // Classe BlackJack - construtor
 class BlackJack {
     constructor() {
         // array com as cartas do dealer
-        this.dealer_cards = [];
+        this.dealer_cards = []
         // array com as cartas do player
-        this.player_cards = [];
+        this.player_cards = []
         // variável booleana que indica a vez do dealer jogar até ao fim
-        this.dealerTurn = false;
+        this.dealerTurn = false
 
         // objeto na forma literal com o estado do jogo
         this.state = {
@@ -23,58 +23,58 @@ class BlackJack {
 
         //métodos utilizados no construtor (DEVEM SER IMPLEMENTADOS PELOS ALUNOS)
         this.new_deck = function () {
-            let suits = ["spades", "hearts", "diamonds", "clubs"];
-            let values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+            let suits = ["spades", "hearts", "diamonds", "clubs"]
+            let values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
             // deck_aux = new Array();
-            let deck_aux = [];
+            let deck_aux = []
             for (var i = 0; i < values.length; i++) {
                 for (var x = 0; x < suits.length; x++) {
                     /*
                     var weight = parseInt(values[i]);
                     if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
-                        weight = 10;
+                        weight = 10
                     if (values[i] == "A")
-                        weight = 11;*/
-                    var card = { Value: values[i], Suit: suits[x] };
+                        weight = 11*/
+                    var card = { Value: values[i], Suit: suits[x] }
                     //var card = values[i] + "_of_" + suits[x];
-                    deck_aux.push(card);
+                    deck_aux.push(card)
                 }
             }
-            return deck_aux;
+            return deck_aux
 
         };
 
         this.shuffle = function (deck) {
-            let shuffledDeck = [];
-            let indices = Array.from({ length: deck.length }, (_, i) => i);
+            let shuffledDeck = []
+            let indices = Array.from({ length: deck.length }, (_, i) => i)
 
             while (indices.length > 0) {
-                const randomIndex = Math.floor(Math.random() * indices.length);
-                const cardIndex = indices.splice(randomIndex, 1)[0];
-                shuffledDeck.push(deck[cardIndex]);
+                const randomIndex = Math.floor(Math.random() * indices.length)
+                const cardIndex = indices.splice(randomIndex, 1)[0]
+                shuffledDeck.push(deck[cardIndex])
             }
 
             return shuffledDeck;
         };
 
         // baralho de cartas baralhado
-        this.deck = this.shuffle(this.new_deck());
+        this.deck = this.shuffle(this.new_deck())
     }
 
     // métodos
     // devolve as cartas do dealer num novo array (splice)
     get_dealer_cards() {
-        return this.dealer_cards.slice();
+        return this.dealer_cards.slice()
     }
 
     // devolve as cartas do player num novo array (splice)
     get_player_cards() {
-        return this.player_cards.slice();
+        return this.player_cards.slice()
     }
 
     // Ativa a variável booleana "dealerTurn"
     setDealerTurn(val) {
-        this.dealerTurn = val;
+        this.dealerTurn = val
     }
 
     //MÉTODOS QUE DEVEM SER IMPLEMENTADOS PELOS ALUNOS
@@ -82,41 +82,41 @@ class BlackJack {
         /*
         for (var i = 0; i < values.length; i++) {
             for (var x = 0; x < suits.length; x++) {
-                let value = parseInt(values[i]);
+                let value = parseInt(values[i])
                     if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
-                        value = 10;
+                        value = 10
                     if (values[i] == "A")
-                        value = 11;
+                        value = 11
                 
             }
         }*/
         let valor = 0;
         for (const card of cards) {
-            let value = card.Value;
+            let value = card.Value
 
             if (value === "A") {
-                numAces++;
-                valor += 11;
+                numAces++
+                valor += 11
             } else if (value === "K" || value === "Q" || value === "J") {
-                valor += 10;
+                valor += 10
             } else {
-                valor += parseInt(value);
+                valor += parseInt(value)
             }
         }
-        return valor;
+        return valor
     }
 
     dealer_move() {
         var carta= this.deck.pop()
         this.dealer_cards.push(carta)
-        //this.dealerTurn = true;
+        //this.dealerTurn = true
 
     }
 
     player_move() {
         var carta= this.deck.pop()
         this.player_cards.push(carta)
-        //this.dealerTurn = false;
+        //this.dealerTurn = false
         this.get_game_state()
     }
 
@@ -142,7 +142,7 @@ class BlackJack {
                 //continuar ou n
 
             }*/
-            return this.state;
+            return this.state
         }
         else{
             return this.state
