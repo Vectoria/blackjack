f09b44eddfd31923ed329ad41ded868a6ec71e5d
deSoos <fsousa2004@gmail.com>
2023-11-02 14:44:48 +0000

    não sei, mas está diferente

--- js/blackjack_manager.js
+++ js/blackjack_manager.js
@@ -39,31 +39,6 @@ function update_dealer(state) {
             dealer_info += ", "
         }
     }
-    if (state == game.state.gameEnded) {
-        let dealer_cards = game.get_dealer_cards()
-        dealer_info += "Dealer Cards: "
-        for (let i = 0; i < dealer_cards.length; i++) {
-            dealer_info += dealer_cards[i].Value + " of " + dealer_cards[i].Suit
-            if (i < dealer_cards.length - 1) {
-                dealer_info += ", "
-            }
-        }
-        if (state.dealerWon) {
-            dealer_info += "dealer wins"
-        } else {
-            dealer_info += "dealer loses"
-        }
-        document.getElementById("dealer_cards").innerHTML = dealer_info
-        finalize_buttons()
-    }
-
-
-    for (let i = 0; i < dealer_cards.length; i++) {
-        dealer_info += dealer_cards[i].Value + " of " + dealer_cards[i].Suit
-        if (i < dealer_cards.length - 1) {
-            dealer_info += ", "
-        }
-    }
 
     if (state.gameEnded) {
         if (state.dealerWon) {
@@ -75,12 +50,7 @@ function update_dealer(state) {
     }
     document.getElementById("dealer_cards").innerHTML = dealer_info
 }
-/*
-function update_player(state) {
-    let playerCards = game.get_player_cards()
 
-    //dealer_move()
-}*/
 function update_player(state) {
     //ideia: por um h1 ou p na direita, onde será o valor total
     let player_info = ""
@@ -101,51 +71,41 @@ function update_player(state) {
             }
         }
         if (state.dealerWon) {
-
             player_info += " - Player loses"
         } else {
             player_info += " - Player won"
         }
-
         finalize_buttons()
     }
     document.getElementById("player_cards").innerHTML = player_info
 }
-
-
 function dealer_new_card() {
-
-    game.dealer_move();
-    let dealerCards = game.get_dealer_cards();
-    console.log("Dealer's new card:", dealerCards[dealerCards.length - 1]);
-    update_dealer(game.get_game_state());
-    return game.get_game_state();
-
+    game.dealer_move()
+    let dealerCards = game.get_dealer_cards()
+    console.log("Dealer's new card:", dealerCards[dealerCards.length - 1])
+    update_dealer(game.get_game_state())
+    return game.get_game_state()
 }
 
 function player_new_card() {
     game.player_move()
-
-    update_player(game.get_game_state());
-
+    update_player(game.get_game_state())
     //game.get_cards_value(game.get_player_cards)
     return game.get_game_state()
 }
 
 function dealer_finish() {
-
-  //TODO
-    console.log("Dealer's turn started");
-    game.setDealerTurn(true);
-    let state = game.get_game_state();
+    console.log("Dealer's turn started")
+    game.setDealerTurn(true)
+    let state = game.get_game_state()
     
     while (!state.gameEnded && game.get_cards_value(game.get_dealer_cards()) < MIN_POINTS_DEALER) {
-        console.log("Drawing a new card for the dealer");
-        dealer_new_card();
-        state = game.get_game_state();
-        console.log("Dealer's state:", state);
+        console.log("Drawing a new card for the dealer")
+        dealer_new_card()
+        state = game.get_game_state()
+        console.log("Dealer's state:", state)
         update_dealer(state);
     }
     
-    console.log("Dealer's turn ended");
+    console.log("Dealer's turn ended")
 }
